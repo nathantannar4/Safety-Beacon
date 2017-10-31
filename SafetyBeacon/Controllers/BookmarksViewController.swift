ebe98bd8593e1fce834c9c3068fc26c1c071cf2f
Jason Tsang <jasonrtsang@gmail.com>
2017-10-30 23:20:01 -0700

    Add coversion from coordinates to address

--- SafetyBeacon/Controllers/BookmarksViewController.swift
+++ SafetyBeacon/Controllers/BookmarksViewController.swift
@@ -7,6 +7,7 @@
 //  Copyright © 2017 Nathan Tannar. All rights reserved.
 //
 
+import Contacts
 import CoreLocation
 import AddressBookUI
 import NTComponents
@@ -24,6 +25,7 @@ class BookmarksViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Bookmarks"
+        self.refreshBookmarks()
     }
     
     func refreshBookmarks() {
@@ -77,14 +79,22 @@ class BookmarksViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
         
-        if (indexPath.section == 0) {
+        if indexPath.section == 0 {
             cell.textLabel?.text = "Enter Address"
             cell.accessoryType = .disclosureIndicator
             return cell
         }
         
         cell.textLabel?.text = bookmarks[indexPath.row]["name"] as? String
-        //        cell.detailTextLabel?.text = safeZones[indexPath.row]["name"] as? String
+        let latitude = bookmarks[indexPath.row]["lat"] as? Double
+        let longitude = bookmarks[indexPath.row]["long"] as? Double
+        self.getAddress(latitude: latitude!, longitude: longitude!, completion: { (address) in
+            guard let address = address else {
+                // handle error
+                return
+            }
+            cell.detailTextLabel?.text = address
+        })
         return cell
         
     }
@@ -92,7 +102,7 @@ class BookmarksViewController: UITableViewController {
     // MARK: - UITableViewDelegate
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if (indexPath.section == 0) {
+        if indexPath.section == 0 {
             addBookmark()
         }
     }
@@ -117,7 +127,6 @@ class BookmarksViewController: UITableViewController {
     // MARK: - User Actions
     func getCoordinates(address: String, completion: @escaping (CLLocationCoordinate2D?) -> Void) {
         CLGeocoder().geocodeAddressString(address, completionHandler: { (placemarks, error) in
-            
             if error != nil {
                 print(error as Any)
                 return
@@ -126,25 +135,55 @@ class BookmarksViewController: UITableViewController {
                 let placemark = placemarks?[0]
                 let location = placemark?.location
                 let coordinate = location?.coordinate
-                print("\nlat: \(coordinate!.latitude), long: \(coordinate!.longitude)")
-                if placemark?.areasOfInterest?.count != nil {
-                    let areaOfInterest = placemark!.areasOfInterest![0]
-                    print(areaOfInterest)
-                    completion(coordinate)
-                } else {
-                    print("No area of interest found.")
-                    completion(nil)
-                }
+//                print("\nlat: \(coordinate!.latitude), long: \(coordinate!.longitude)")
+//                if placemark?.areasOfInterest?.count != nil {
+//                    let areaOfInterest = placemark!.areasOfInterest![0]
+//                    print(areaOfInterest)
+//                } else {
+//                    print("No area of interest found.")
+//                }
+                completion(coordinate)
+            } else {
+                completion(nil)
             }
-            
         })
     }
-
+    
+    func getAddress(latitude: CLLocationDegrees, longitude: CLLocationDegrees, completion: @escaping (String?) -> Void) {
+        let location = CLLocation(latitude: latitude, longitude: longitude)
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location, completionHandler: { (placemarks, error) in
+            if error != nil {
+                print(error as Any)
+                return
+            } else if placemarks?.count != nil {
+                let pm = placemarks![0]
+                guard let streetField = pm.postalAddress?.street,
+                      let cityField = pm.postalAddress?.city,
+                      let provinceField = pm.postalAddress?.state,
+                      let postalField = pm.postalAddress?.postalCode
+                else {
+                    return
+                }
+                let address = "\(streetField), \(cityField), \(provinceField), \(postalField)"
+//                if pm.areasOfInterest?.count != nil {
+//                    let areaOfInterest = pm.areasOfInterest?[0]
+//                    print(areaOfInterest!)
+//                } else {
+//                    print("No area of interest found.")
+//                }
+                completion(address)
+            } else {
+                completion(nil)
+            }
+        })
+    }
+    
     func addBookmark() {
         let alertController = UIAlertController(title: "Add Bookmark", message: "Input Bookmark name and address below:", preferredStyle: UIAlertControllerStyle.alert)
-        
+
         let addAction = UIAlertAction(title: "Add", style: UIAlertActionStyle.default) { (alertAction: UIAlertAction!) -> Void in
-            
+
             guard let nameField = alertController.textFields?[0].text,
                   let streetField = alertController.textFields?[1].text,
                   let cityField = alertController.textFields![2].text,
@@ -153,10 +192,8 @@ class BookmarksViewController: UITableViewController {
             else {
                 return
             }
-            
             let address = "\(streetField), \(cityField), \(provinceField), \(postalField)"
-            print ("\(address)")
-            
+
             self.getCoordinates(address: address, completion: { (coordinate) in
                 guard let coordinate = coordinate else {
                     // handle error
@@ -166,7 +203,7 @@ class BookmarksViewController: UITableViewController {
             })
         }
 //        addAction.isEnabled = false
-        
+    
         alertController.addTextField { nameField in nameField.placeholder = "Bookmark Name" }
         alertController.addTextField { streetField in streetField.placeholder = "Street Address" }
         alertController.addTextField { cityField in cityField.placeholder = "City" }
@@ -174,20 +211,20 @@ class BookmarksViewController: UITableViewController {
         alertController.addTextField { postalField in postalField.placeholder = "Postal Code" }
 
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        
+
         alertController.addAction(addAction)
         alertController.addAction(cancelAction)
         present(alertController, animated: true, completion: nil)
-        
+    
     }
     
     func saveBookmark(name: String, addressLatitude: Double, addressLongitude: Double) {
         guard let currentUser = User.current(), currentUser.isCaretaker, let patient = currentUser.patient else { return }
         
         let bookmark = PFObject(className: "Bookmarks")
-        bookmark["long"] = addressLatitude
-        bookmark["lat"] = addressLongitude
-        bookmark["name"] = "Test"
+        bookmark["lat"] = addressLatitude
+        bookmark["long"] = addressLongitude
+        bookmark["name"] = name
         bookmark["patient"] = patient
         bookmark.saveInBackground { (success, error) in
             guard success else {
