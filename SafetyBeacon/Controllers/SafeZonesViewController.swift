9e9efbb737c2ac758ee94be0bdca43195eea049c
jshercli <jshercli@asb9700-e06.csil.sfu.ca>
2017-11-04 15:21:52 -0700

    Safezones WIP

--- SafetyBeacon/Controllers/SafeZonesViewController.swift
+++ SafetyBeacon/Controllers/SafeZonesViewController.swift
@@ -1,30 +1,57 @@
 //
-//  SafeZonesViewController.swift
+//  SafeZones.swift
 //  SafetyBeacon
 //
-//  Created by Nathan Tannar on 10/2/17.
+//  Created by Nathan Tannar on 9/25/17
+//  Implemented by Jason Tsang on 10/29/2017
 //  Copyright © 2017 Nathan Tannar. All rights reserved.
 //
 
-import UIKit
+import AddressBookUI
+import Contacts
+import CoreLocation
 import NTComponents
 import Parse
-import CoreLocation
-import AddressBookUI
+import UIKit
 
-class SafeZonesViewController: UITableViewController {
+class SafeZonesViewController: UITableViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     // MARK: - Properties
     
     var safeZones = [PFObject]()
     
+    var provinceOption = ["BC", "AB", "SK", "MB", "ON"]
+    let thePicker = UIPickerView()
+    var pickerTextField: String = ""
+    
     // MARK: - View Life Cycle
     
+    // Initial load confirmation
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Safe Zones"
+        self.refreshSafeZones()
         
+        thePicker.delegate = self
     }
     
+    // MARK: - UIPickerView
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return provinceOption.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return provinceOption[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        pickerTextField = provinceOption[row]
+        print("\(pickerTextField)")
+    }
+    
+    // Updating Safe Zones from database
     func refreshSafeZones() {
         
         //query db
@@ -40,16 +67,26 @@ class SafeZonesViewController: UITableViewController {
             self.safeZones = objects
             self.tableView.reloadData()
         }
-        
-        
     }
-    
     // MARK: - UITableViewDataSource
     
+    // Sections within Bookmarks View
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 2
     }
     
+    // Section titles
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = NTTableViewHeaderFooterView()
+        if section == 0 {
+            header.textLabel.text = "Add Safe Zones"
+        } else if section == 1 {
+            header.textLabel.text = "Existing Safe Zones"
+        }
+        return header
+    }
+    
+    // Section rows
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if section == 0 {
             return 1
@@ -57,83 +94,147 @@ class SafeZonesViewController: UITableViewController {
         return safeZones.count
     }
     
+    // Table styling
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 44
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return indexPath.section <= 1 ? 80 : UITableViewAutomaticDimension
+    }
+    
+    // Populating row content
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
         
+        // Add Bookmarks
         if indexPath.section == 0 {
-            cell.textLabel?.text = "Add Safe Zone"
+            cell.textLabel?.text = "Enter Address"
             cell.accessoryType = .disclosureIndicator
             return cell
         }
         
-        
+        // Existing Bookmarks
         cell.textLabel?.text = safeZones[indexPath.row]["name"] as? String
-//        cell.detailTextLabel?.text = safeZones[indexPath.row]["name"] as? String
+        cell.detailTextLabel?.text = safeZones[indexPath.row]["address"] as? String
         return cell
-        
     }
-    
     
     // MARK: - UITableViewDelegate
     
+    // Row selectable actions
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.section == 0 {
             addSafeZone()
         }
     }
     
+    // Modifiable rows (not first section)
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return indexPath.section != 0
     }
     
-//    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-//        let action = UITableViewRowAction(style: UITableViewRowActionStyle.destructive, title: "Delete") { (action, indexPath) in
-//            self.safeZones[indexPath.row].deleteInBackground(block: { (success, error) in
-//                guard success else {
-//                    NTPing(type: .isDanger, title: "Unable to delete Safe Zone").show(duration: 5)
-//                    Log.write(.error, error.debugDescription)
-//                    return
-//                }
-//                self.safeZones.remove(at: indexPath.row)
-//                self.tableView.deleteRows(at: [indexPath], with: .fade)
-//            })
-//        }
-//        return action[]
-//    }
-    
-    // MARK: - User Actions
-    
-    @objc
-    func addSafeZone() {
-        let alertController = UIAlertController(title: "Add SafeZone", message: "Input Safe Zone Information Below", preferredStyle: UIAlertControllerStyle.alert)
-        let addAction = UIAlertAction(title: "Add", style: UIAlertActionStyle.default) { (alertAction: UIAlertAction) -> Void in
-            guard let nameField = alertController.textFields?[0].text, let streetField = alertController.textFields?[1].text, let cityField = alertController.textFields?[2].text, let provinceField = alertController.textFields?[3].text, let postalField = alertController.textFields?[4].text, let radiusField = alertController.textFields?[5].text else{
-                return
-            }
-            guard let radius = Double(radiusField) else { return }
-            let address = "\(streetField ?? "Null"), \(cityField ?? "Null"), \(provinceField ?? "Null"), \(postalField ?? "Null")"
-            print ("\(address)")
-            self.getCoordinates(address: address, completion: { (coordinate) in
-                guard let coordinate = coordinate else {
-                    // handle error
+    // Modifiable row actions (swipe left)
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let delete = UITableViewRowAction(style: UITableViewRowActionStyle.destructive, title: "Delete") { (_, indexPath) in
+            self.safeZones[indexPath.row].deleteInBackground(block: { (success, error) in
+                guard success else {
+                    Log.write(.error, error.debugDescription)
+                    NTPing(type: .isDanger, title: error?.localizedDescription).show(duration: 3)
                     return
                 }
-                self.saveSafeZone(safeZoneName: nameField, safeZoneAddress: address, safeZoneRadius: radius, safeZoneLatitude: coordinate.latitude, safeZoneLongitude: coordinate.longitude)
+                self.safeZones.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+                NTPing(type: .isSuccess, title: "Safe Zone successfully deleted").show(duration: 3)
             })
         }
-        alertController.addTextField { nameField in nameField.placeholder = "Safe Zone Name" }
-        alertController.addTextField { streetField in streetField.placeholder = "Street Address" }
-        alertController.addTextField { cityField in cityField.placeholder = "City" }
-        alertController.addTextField { provinceField in provinceField.placeholder = "Province/ Territory" }
-        alertController.addTextField { postalField in postalField.placeholder = "Postal Code" }
-        alertController.addTextField { postalField in postalField.placeholder = "Safe Zone Radius" }
         
-        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil)
-        alertController.addAction(addAction)
-        alertController.addAction(cancelAction)
-        present(alertController, animated: true, completion: nil)
-        
+        let edit = UITableViewRowAction(style: UITableViewRowActionStyle.normal, title: "Edit") { (_, indexPath) in
+            let alertController = UIAlertController(title: "Edit Safe Zone", message: "Change Safe Zone name and address:", preferredStyle: UIAlertControllerStyle.alert)
+            
+            // Get original bookmark
+            let originalName = self.safeZones[indexPath.row]["name"] as? String
+            let originalRadius = self.safeZones[indexPath.row]["radius"] as? String
+            let concatenatedAddress = self.safeZones[indexPath.row]["address"] as? String
+            var concatenatedAddressArr = concatenatedAddress?.components(separatedBy: ", ")
+            let originalStreet = concatenatedAddressArr![0] as String
+            let originalCity = concatenatedAddressArr![1] as String
+            let originalProvince = concatenatedAddressArr![2] as String
+            let originalPostal = concatenatedAddressArr![3] as String
+            
+            // Text input placeholders
+            alertController.addTextField { nameField in nameField.placeholder = "Safe Zone Name" }
+            alertController.addTextField { streetField in streetField.placeholder = "Street Address" }
+            alertController.addTextField { cityField in cityField.placeholder = "City" }
+            alertController.addTextField { provinceField in provinceField.placeholder = "Province/ Territory" }
+            alertController.addTextField { postalField in postalField.placeholder = "Postal Code" }
+            alertController.addTextField { radiusField in radiusField.placeholder = "Radius Field" }
+            // Put original row content as text input
+            alertController.textFields![0].text = originalName
+            alertController.textFields![1].text = originalStreet
+            alertController.textFields![2].text = originalCity
+            alertController.textFields![3].text = originalProvince
+            alertController.textFields![4].text = originalPostal
+            alertController.textFields![5].text = originalRadius
+            
+            // Change button
+            let changeAction = UIAlertAction(title: "Change", style: UIAlertActionStyle.default) { (_: UIAlertAction!) -> Void in
+                guard let nameField = alertController.textFields![0].text, !nameField.isEmpty,
+                    let streetField = alertController.textFields![1].text, !streetField.isEmpty,
+                    let cityField = alertController.textFields![2].text, !cityField.isEmpty,
+                    let provinceField = alertController.textFields![3].text, !provinceField.isEmpty,
+                    let postalField = alertController.textFields![4].text, !postalField.isEmpty,
+                    let radiusField = alertController.textFields![5].text, !radiusField.isEmpty
+                    else {
+                        let invalidAlert = UIAlertController(title: "Invalid Safe Zone", message: "All fields must be entered.", preferredStyle: .alert)
+                        invalidAlert.addAction(UIAlertAction(title: NSLocalizedString("Ok", comment: "Default action"), style: .`default`, handler: { _ in
+                            NSLog("The \"Invalid Safe Zone\" alert occured.")
+                        }))
+                        self.present(invalidAlert, animated: true, completion: nil)
+                        return
+                }
+                
+                // Update only address fields changed
+                if originalStreet != streetField || originalCity != cityField || originalProvince != provinceField || originalPostal != postalField {
+                    let newAddress = "\(streetField), \(cityField), \(provinceField), \(postalField)"
+                    // Convert address to coordinates
+                    self.getCoordinates(address: newAddress, completion: { (coordinate) in
+                        guard let coordinate = coordinate else {
+                            print("\n Failed to getCoordinates() - \(newAddress)")
+                            NTPing(type: .isDanger, title: "Invalid Address").show(duration: 5)
+                            return
+                        }
+                        self.safeZones[indexPath.row]["lat"] = coordinate.latitude
+                        self.safeZones[indexPath.row]["long"] = coordinate.longitude
+                    })
+                    self.safeZones[indexPath.row]["address"] = newAddress
+                }
+                // Update if name changed
+                if originalName != nameField {
+                    self.safeZones[indexPath.row]["name"] = nameField
+                }
+                if originalRadius != radiusField {
+                    self.safeZones[indexPath.row]["radius"] = radiusField
+                }
+                
+                // Save bookmark at same row
+                self.safeZones[indexPath.row].saveInBackground()
+                self.refreshSafeZones()
+                NTPing(type: .isSuccess, title: "Safe Zone successfully updated").show(duration: 3)
+            }
+            
+            // Cancel button
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            
+            alertController.addAction(changeAction)
+            alertController.addAction(cancelAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        return [delete, edit]
     }
+    
+    // MARK: - Processing Functions
+    
+    // Get address from coordinates
     func getCoordinates(address: String, completion: @escaping (CLLocationCoordinate2D?)->Void) {
         CLGeocoder().geocodeAddressString(address, completionHandler: { (placemarks, error) in
             
@@ -158,26 +259,87 @@ class SafeZonesViewController: UITableViewController {
             
         })
     }
-    func saveSafeZone(safeZoneName: String, safeZoneAddress: String, safeZoneRadius: Double, safeZoneLatitude : Double, safeZoneLongitude: Double) {
+    
+    // Save safe zone to database
+    func safeSafeZone(name: String, addressConcatinated: String, addressLatitude: Double, addressLongitude: Double, addressRadius: Double) {
         
+        // Check that Caretaker is accessing this menu, not Patient
         guard let currentUser = User.current(), currentUser.isCaretaker, let patient = currentUser.patient else { return }
         
-        let zone = PFObject(className: "SafeZones")
-        //zone["lat"] = LocationManager.shared.currentLocation?.latitude
-        zone["address"] = safeZoneAddress
-        zone["name"] = safeZoneName
+        let zone = PFObject(className: "SafeZone")
+        zone["name"] = name
+        zone["address"] = addressConcatinated
+        zone["lat"] = addressLatitude
+        zone["long"] = addressLongitude
         zone["patient"] = patient
-        zone["radius"] = safeZoneRadius
+        zone["radius"] = addressRadius
         zone.saveInBackground { (success, error) in
             guard success else {
-                // handle error
                 Log.write(.error, error.debugDescription)
                 NTPing(type: .isDanger, title: error?.localizedDescription).show(duration: 3)
                 return
             }
-            NTPing(type: .isSuccess, title: "Safe Zone Successfully Saved").show(duration: 3)
+            self.refreshSafeZones()
+            NTPing(type: .isSuccess, title: "Safe Zone successfully saved").show(duration: 3)
         }
     }
+    
+    // MARK: - User Actions
+    
+    // Add safe zone
+    func addSafeZone() {
+        let alertController = UIAlertController(title: "Add Safe Zone", message: "Input Safe Zone name and address:", preferredStyle: UIAlertControllerStyle.alert)
+        
+        // Text input placeholders
+        alertController.addTextField { nameField in nameField.placeholder = "Safe Zone Name" }
+        alertController.addTextField { streetField in streetField.placeholder = "Street Address" }
+        alertController.addTextField { cityField in cityField.placeholder = "City" }
+        alertController.addTextField { provinceField in provinceField.placeholder = "Province/ Territory"
+            provinceField.inputView = self.thePicker
+        }
+        alertController.addTextField { postalField in postalField.placeholder = "Postal Code" }
+        alertController.addTextField { radiusField in radiusField.placeholder = "Radius Field" }
+        
+        // Add button
+        let addAction = UIAlertAction(title: "Add", style: UIAlertActionStyle.default) { (_: UIAlertAction!) -> Void in
+            guard let nameField = alertController.textFields?[0].text, !nameField.isEmpty,
+                let streetField = alertController.textFields?[1].text, !streetField.isEmpty,
+                let cityField = alertController.textFields?[2].text, !cityField.isEmpty,
+                let radiusField = alertController.textFields?[3].text, !radiusField.isEmpty,
+                let postalField = alertController.textFields?[4].text, !postalField.isEmpty
+                else {
+                    let invalidAlert = UIAlertController(title: "Invalid Safe Zone", message: "All fields must be entered.", preferredStyle: .alert)
+                    invalidAlert.addAction(UIAlertAction(title: NSLocalizedString("Ok", comment: "Default action"), style: .`default`, handler: { _ in
+                        NSLog("The \"Invalid Safe Zone\" alert occured.")
+                    }))
+                    self.present(invalidAlert, animated: true, completion: nil)
+                    return
+            }
+            
+            // How do I get textFields[3].text to update to what was in the pickerTextField (i.e. show what was chosen from the picker)
+            alertController.textFields?[3].text = self.pickerTextField
+            
+            let address = "\(streetField), \(cityField), \(self.pickerTextField), \(postalField)"
+            
+            // Convert address to coordinates
+            self.getCoordinates(address: address, completion: { (coordinate) in
+                guard let coordinate = coordinate else {
+                    print("\n Failed to getCoordinates() - lat: \(address)")
+                    NTPing(type: .isDanger, title: "Invalid Address").show(duration: 5)
+                    return
+                }
+                // Save to database
+                guard let radius = Double(radiusField) else { return }
+                self.safeSafeZone(name: nameField, addressConcatinated: address, addressLatitude: coordinate.latitude, addressLongitude: coordinate.longitude, addressRadius: radius)
+            })
+        }
+        
+        // Cancel button
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(addAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
 }
-
 
