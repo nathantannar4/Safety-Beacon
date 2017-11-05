792236be673fca7b5ba8fb1495b0f00ec72432ef
Nathan Tannar <nathantannar4@gmail.com>
2017-11-04 22:22:55 -0800

    Add some tests

--- SafetyBeaconTests/SafetyTrackerTests.swift
+++ SafetyBeaconTests/SafetyTrackerTests.swift
@@ -19,5 +19,31 @@ class SafetyTrackerTests: XCTestCase {
         super.tearDown()
     }
     
+    func testCorrectLogin() {
+        User.loginInBackground(email: "caretaker@safetybeacon.ca", password: "password123") { (success) in
+            XCTAssert(success)
+        }
+    }
+    
+    func testIncorrectLogin() {
+        User.loginInBackground(email: "caretaker@safetybeacon.ca", password: "wrongpassword") { (success) in
+            // Success should be false
+            XCTAssert(!success)
+        }
+    }
+    
+    func testCorrectRegister() {
+        let randomEmail = String.random(ofLength: 12) + "safetybeacon.ca"
+        User.loginInBackground(email: randomEmail, password: String.random(ofLength: 8)) { (success) in
+            XCTAssert(success)
+        }
+    }
+    
+    func testIncorrectRegister() {
+        User.registerInBackground(email: "caretaker@safetybeacon.ca", password: "password123") { (success) in
+            // User already in use
+            XCTAssert(!success)
+        }
+    }
     
 }
