fbbfb2367c2e9fe87105a9c521fd536ed8928bf5
Jason Tsang <jasonrtsang@gmail.com>
2017-11-18 19:32:58 -0800

    Bookmarks view for patient corrected

--- SafetyBeacon/Controllers/BookmarksNavigationViewController.swift
+++ SafetyBeacon/Controllers/BookmarksNavigationViewController.swift
@@ -44,19 +44,7 @@ class BookmarksNavigationViewController: UITableViewController {
         guard let currentUser = User.current(), currentUser.isPatient else { return }
         
         let query = PFQuery(className: "Bookmarks")
-print("\n currentUser is - \(currentUser)")
-query.whereKey("patient", equalTo: currentUser)
-
-//SIGABT from line above ^
-//        currentUser is - <PFUser: 0x6000000e1080, objectId: pRGYHaleS6, localId: (null)> {
-//            ACL = "<PFACL: 0x600000431180>";
-//            caretaker = "<PFUser: 0x6000000e7500, objectId: iMZAoyqRS0, localId: (null)>";
-//            email = "patient@safetybeacon.ca";
-//            emailVerified = 1;
-//            username = "patient@safetybeacon.ca";
-//        }
-//        2017-11-17 00:08:37.950949-0800 SafetyBeacon[13300:1847179] *** Terminating app due to uncaught exception 'NSInvalidArgumentException', reason: 'Cannot do a comparison query for type: SafetyBeacon.User'
-
+        query.whereKey("patient", equalTo: currentUser.object)
         query.findObjectsInBackground { (objects, error) in
             self.tableView.refreshControl?.endRefreshing()
             guard let objects = objects else {
